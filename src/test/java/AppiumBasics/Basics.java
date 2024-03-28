6d7a6f2898b5c96f0a2c03362796237e23655c44
surajskhopkar <suraj.khopkar@icloud.com>
2024-03-28 21:16:48 +0530

    added appium service builder

--- src/test/java/AppiumBasics/Basics.java
+++ src/test/java/AppiumBasics/Basics.java
@@ -2,8 +2,10 @@ package AppiumBasics;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.testng.annotations.Test;
-
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -13,11 +15,15 @@ public class Basics {
     @Test
     public void AppiumTest() throws MalformedURLException, URISyntaxException {
 
+        AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(new File("//usr//local//lib//node_modules//appium//build//lib//main.js"))
+                .withIPAddress("127.0.0.1").usingPort(4723).build();
+        service.start();
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("Pixel 6 Pro API 34");
         options.setApp("//Users//surajkhopkar//Library//CloudStorage//OneDrive-Personal//Repositories//JavaFrameworks//AppiumJava//src//test//java//Resources//ApiDemos-debug.apk");
         AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(),options);
         driver.quit();
+        service.stop();
 
     }
 }
