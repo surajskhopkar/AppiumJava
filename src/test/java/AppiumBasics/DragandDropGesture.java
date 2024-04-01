4e6f24613b2d86392e6cb4386b7182897d82f8db
surajskhopkar <suraj.khopkar@icloud.com>
2024-04-01 20:46:26 +0530

    Added drag and drop gesture

---
+++ src/test/java/AppiumBasics/DragandDropGesture.java
@@ -0,0 +1,49 @@
+package AppiumBasics;
+
+import Utils.Conftest;
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DragandDropGesture extends Conftest {
+
+
+    @Test
+    public void DragandDrop() {
+
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        driver.findElement(AppiumBy.accessibilityId("Drag and Drop")).click();
+        WebElement source = driver.findElement(By.id("io.appium.android.apis:id/drag_dot_1"));
+        ((JavascriptExecutor) driver).executeScript("mobile: dragGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) source).getId(),
+                "endX", 842,
+                "endY", 804
+        ));
+
+        Assert.assertEquals(driver.findElement(By.id("io.appium.android.apis:id/drag_result_text")).getAttribute("text"),
+                "Dropped!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
